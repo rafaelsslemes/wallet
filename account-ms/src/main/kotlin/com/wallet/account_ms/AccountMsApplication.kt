939abe0316a792eefd7dd4a77ed0dd592134242a
Rafael Lemes <rafael.sslemes@hotmail.com>
2024-08-09 07:47:56 -0300

    Add project account-ms

---
+++ account-ms/src/main/kotlin/com/wallet/account_ms/AccountMsApplication.kt
@@ -0,0 +1,11 @@
+package com.wallet.account_ms
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AccountMsApplication
+
+fun main(args: Array<String>) {
+	runApplication<AccountMsApplication>(*args)
+}
