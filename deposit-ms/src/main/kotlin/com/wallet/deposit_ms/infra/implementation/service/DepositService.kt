637c8906d15349f2f41f9e03ac6aee1b814c990c
Rafael Lemes <rafael.sslemes@hotmail.com>
2024-08-12 15:12:37 -0300

    Send message after deposit

--- deposit-ms/src/main/kotlin/com/wallet/deposit_ms/infra/implementation/service/DepositService.kt
+++ deposit-ms/src/main/kotlin/com/wallet/deposit_ms/infra/implementation/service/DepositService.kt
@@ -1,5 +1,8 @@
 package com.wallet.deposit_ms.infra.implementation.service
 
+import com.wallet.account_ms.infra.dto.MovementType
+import com.wallet.account_ms.infra.dto.TransactionDto
+import com.wallet.account_ms.infra.dto.TransactionType
 import com.wallet.deposit_ms.entity.Deposit
 import com.wallet.deposit_ms.infra.dto.DepositDto
 import com.wallet.deposit_ms.infra.interfaces.repository.DepositRepositoryInterface
@@ -14,13 +17,18 @@ class DepositService(
     val messageServiceInterface: MessageServiceInterface
 ): DepositServiceInterface {
 
-    private final val topic = "transaction"
+    private final val topic = "transactions"
 
     override fun create(dto: DepositDto): UUID {
         val registered = depositRepositoryInterface.save(Deposit(null, dto.userId, dto.value))
 
-        messageServiceInterface.sendMessage(topic, registered)
-
+        try {
+            messageServiceInterface.sendMessage(topic, TransactionDto(
+                registered.userId, registered.value, TransactionType.DEPOSIT, MovementType.CREDIT)
+            )
+        }catch (e:Exception){
+            println(e)
+        }
         return registered.id!!
     }
 }
