09522273fecef0b742a1d720b01e6d61055ebe38
Rafael Lemes <rafael.sslemes@hotmail.com>
2024-08-12 15:12:02 -0300

    remove unused

--- account-ms/src/main/kotlin/com/wallet/account_ms/infra/dto/BalanceUpdateDto.kt
+++
@@ -1,9 +0,0 @@
-package com.wallet.account_ms.infra.dto
-
-import java.util.UUID
-
-class BalanceUpdateDto (
-    val userId: UUID,
-    val transactionType: TransactionType,
-    val value: Double){
-}
